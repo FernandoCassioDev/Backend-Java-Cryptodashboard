c7143ad5820c66f66e06b7f179f1bc013d12bc98
Fernando <fernandocassiodev@gmail.com>
2024-09-24 20:35:38 -0300

    first commit

---
+++ src/main/java/com/cryptodashboard/Cryptodashboard/CryptodashboardApplication.java
@@ -0,0 +1,13 @@
+package com.cryptodashboard.Cryptodashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptodashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptodashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cryptodashboard/Cryptodashboard/CryptodashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryptodashboard.Cryptodashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptodashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
