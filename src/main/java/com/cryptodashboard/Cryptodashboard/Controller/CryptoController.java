3db15df575f5cf6ff07f783ac9b548963700a69f
Fernando <fernandocassiodev@gmail.com>
2024-09-27 12:06:08 -0300

    docs: adiciona comentários informativos

--- src/main/java/com/cryptodashboard/Cryptodashboard/Controller/CryptoController.java
+++ src/main/java/com/cryptodashboard/Cryptodashboard/Controller/CryptoController.java
@@ -12,20 +12,27 @@ import com.cryptodashboard.Cryptodashboard.service.CryptoService;
 
 import reactor.core.publisher.Mono;
 
+//Define a classe como um controlador REST
 @RestController
 public class CryptoController {
 
   private final CryptoService cryptoService;
 
+  //Construtor que injeta o serviço de criptomoedas
   public CryptoController(CryptoService cryptoService) {
     this.cryptoService = cryptoService;
   }
 
+  //Define o endpoint GET
   @GetMapping("/cryptos")
   public Mono<List<Map<String, Object>>> getCryptos(
+    //Recebe a chave da API no cabeçalho da requisição
     @RequestHeader("X-coinAPI-Key") String apiKey,
+    //recebe o valor mínimo da criptomoeda via query param,
+    //com o valor padrão 10
     @RequestParam(defaultValue = "10") double minPrice) {
 
+    //chama o serviço para obter as criptomoedas filtradas
     return cryptoService.getCryptoPricesInUSD(apiKey, minPrice);
   }
 
--- src/main/java/com/cryptodashboard/Cryptodashboard/service/CryptoService.java
+++ src/main/java/com/cryptodashboard/Cryptodashboard/service/CryptoService.java
@@ -10,28 +10,41 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 import reactor.core.publisher.Mono;
 
+// Define esta classe como um serviço no contexto do spring
 @Service
 public class CryptoService {
+
   private final WebClient webClient;
 
+  // Construtor que inicializa o WebClient com a URL base da CoinAPI
   public CryptoService(WebClient.Builder webClientBuilder) {
-    this.webClient = webClientBuilder.baseUrl("https://rest.coinapi.io") // aumenta o limite de buffer para 10mb
+    this.webClient = webClientBuilder.baseUrl("https://rest.coinapi.io") // aumenta o limite de buffer para 20mb
         .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(20 * 1024 * 1024)).build();
   }
 
+  // método que faz a requisição para API e retorna todas as criptomoedas
   public Mono<List<Map<String, Object>>> getAllCryptos(String apiKey) {
     // Fazendo a requisição para a API
-    return this.webClient.get().uri("/v1/assets").header("X-CoinAPI-Key", apiKey)
-        // filtra apenas criptomoedas
-        .retrieve().bodyToMono(new ParameterizedTypeReference<List<Map<String, Object>>>() {
-        }).map(assets ->  assets.stream().filter(asset -> asset.get("price_usd") != null).collect(Collectors.toList()));
+    return this.webClient.get()
+        // EndPoint que retorna todos os ativos
+        .uri("/v1/assets")
+        // Autenticação via chave da API
+        .header("X-CoinAPI-Key", apiKey)
+        // Recupera a resposta
+        .retrieve()
+        .bodyToMono(new ParameterizedTypeReference<List<Map<String, Object>>>() {
+        })
+        // Filtra apenas ativos que possuem preço em USD
+        .map(assets -> assets.stream().filter(asset -> asset.get("price_usd") != null).collect(Collectors.toList()));
   }
 
-   // obtem as criptos acima do preço min em USD
+  // Método que retorna as criptomoedas com preços acima de um valor específico
   public Mono<List<Map<String, Object>>> getCryptoPricesInUSD(String apiKey, double minPrice) {
     return getAllCryptos(apiKey) // obtem todas as criptomoedas
         .map(assets -> assets.stream().filter(asset -> {
-          // garante que o preço realmente está em USD
+
+          // Verifica se o preço em USD existe e é maior que o valor minimo
+
           Double price = (Double) asset.get("price_usd");
           return price != null && price > minPrice;
         })
