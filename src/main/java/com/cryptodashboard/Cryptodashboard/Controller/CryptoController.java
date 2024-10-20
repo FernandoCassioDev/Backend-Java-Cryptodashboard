1372f7cd1ffda4aef3add7defe7d70005660f81a
Fernando <fernandocassiodev@gmail.com>
2024-10-20 10:42:46 -0300

    style: formata o código

--- src/main/java/com/cryptodashboard/Cryptodashboard/Controller/CryptoController.java
+++ src/main/java/com/cryptodashboard/Cryptodashboard/Controller/CryptoController.java
@@ -20,21 +20,21 @@ public class CryptoController {
 
   private final CryptoService cryptoService;
 
-  //Construtor que injeta o serviço de criptomoedas
+  // Construtor que injeta o serviço de criptomoedas
   public CryptoController(CryptoService cryptoService) {
     this.cryptoService = cryptoService;
   }
 
-  //Define o endpoint GET
+  // Define o endpoint GET
   @GetMapping("/cryptos")
   public Mono<List<Map<String, Object>>> getCryptos(
-    //Recebe a chave da API no cabeçalho da requisição
-    @RequestHeader("X-coinAPI-Key") String apiKey,
-    //recebe o valor mínimo da criptomoeda via query param,
-    //com o valor padrão 10
-    @RequestParam(defaultValue = "10") double minPrice) {
+      // Recebe a chave da API no cabeçalho da requisição
+      @RequestHeader("X-coinAPI-Key") String apiKey,
+      // recebe o valor mínimo da criptomoeda via query param,
+      // com o valor padrão 10
+      @RequestParam(defaultValue = "10") double minPrice) {
 
-    //chama o serviço para obter as criptomoedas filtradas
+    // chama o serviço para obter as criptomoedas filtradas
     return cryptoService.getCryptoPricesInUSD(apiKey, minPrice);
   }
 
